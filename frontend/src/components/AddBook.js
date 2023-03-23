ea2c76c305a43a27ecd07b386efae6032176b37a
Ananya <ananyaraj24@gmail.com>
2023-03-23 14:52:07 +0530

    ipfs

--- frontend/src/components/AddBook.js
+++ frontend/src/components/AddBook.js
@@ -4,7 +4,11 @@ const AddBook  = () => {
 
     return (
         <div className="addbook">
-            <p>Add book</p>
+            <p>Add book</p><br />
+            name <input type="text" /><br /><br />
+            price <input type="number" /><br /><br />
+            file <input type="file" />
+
         </div>
     )
 }
